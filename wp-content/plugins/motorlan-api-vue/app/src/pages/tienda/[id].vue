0d110899158ba0072e72c2189a325f0d5ed8bd43
J. Daniel Herrera <50084441+Anchorquery@users.noreply.github.com>
2025-08-21 12:28:45 -0400

    Add motor detail view and related components

---
+++ wp-content/plugins/motorlan-api-vue/app/src/pages/tienda/[id].vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { useApi } from '@/composables/useApi'
+import { createUrl } from '@/@core/composable/createUrl'
+import type { Motor } from '@/interfaces/motor'
+import ProductImage from './components/ProductImage.vue'
+import ProductDetails from './components/ProductDetails.vue'
+import ProductDocs from './components/ProductDocs.vue'
+import RelatedProducts from './components/RelatedProducts.vue'
+
+const route = useRoute()
+const id = route.params.id as string
+
+const { data, isFetching } = await useApi<any>(
+  createUrl(`/wp-json/motorlan/v1/motors/${id}`)
+).get().json()
+
+const motor = computed(() => data.value?.data as Motor | undefined)
+</script>
+
+<template>
+  <div v-if="motor" class="motor-detail">
+    <div class="d-flex flex-wrap gap-6 mb-8">
+      <ProductImage :motor="motor" />
+      <ProductDetails :motor="motor" />
+    </div>
+    <ProductDocs :docs="motor.acf?.documentacion" class="mb-8" />
+    <RelatedProducts :current-id="motor.id" />
+  </div>
+  <div v-else-if="isFetching" class="text-center pa-12">
+    <VProgressCircular indeterminate size="64" />
+  </div>
+  <VCard v-else class="pa-8 text-center">
+    <VCardText>Motor no encontrado</VCardText>
+  </VCard>
+</template>
+
+<style scoped>
+.motor-detail {
+  max-width: 1200px;
+  margin-inline: auto;
+}
+</style>
